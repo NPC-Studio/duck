e97ef9bb5786034fb3476fea017d540551f29b44
Gabe Weiner <imlazyeye@gmail.com>
2025-04-29 10:46:59 -0400

    update lints

--- src/lint/collection/function_name_as_parameter.rs
+++ src/lint/collection/function_name_as_parameter.rs
@@ -1,10 +1,10 @@
 use codespan_reporting::diagnostic::{Diagnostic, Label};
 
 use crate::{
+    FileId,
     driver::Ctx,
     lint::{LateExprPass, Lint, LintLevel},
     parse::{Expr, ExprKind, Function},
-    FileId,
 };
 
 #[derive(Debug, PartialEq)]
